2902aba4f71be036e34e89560a0b0b4734ea38a5
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-13 16:58:30 +0800

    rename folder

--- base/jest/__test__/link.test.js
+++ base/jest/__test__/link.test.js
@@ -6,7 +6,7 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Link page="http://www.facebddook.com">Facebook</Link>)
+    .create(<Link page="http://www.taobao.com">Facebook</Link>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
