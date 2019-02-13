65693f80306d8bc042d69e27439ada30d82de896
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-13 17:18:53 +0800

    react-feature hooks and suspense add

---
+++ base/react-feature/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div class="App">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+export default App;
