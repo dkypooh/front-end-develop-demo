d74a8b7b8eb3e98728de90057316aa9dac41e00d
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-15 16:42:45 +0800

    add protocol

---
+++ project/protocol/src/namespace/conversation.js
@@ -0,0 +1,15 @@
+
+  /**
+   * @namespace
+   * @name Conversation
+   * @property {String}   id                  会话ID
+   * @property {String}   scene               {@link IMMessage.type|场景}
+   * @property {String}   to                  聊天对象, 账号或群ID
+   * @property {Long}     updateTime          会话更新的时间
+   * @property {Number}   unread              未读数
+   * @property {Message}  lastMsg             此会话的最后一条消息
+   * @property {String}   custom              自定义消息
+   */
+
+
+   
