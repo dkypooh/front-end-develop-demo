1bb7d05a0347ba4ed597401083bed254ff724b31
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-17 17:55:08 +0800

    feat: add done functin

--- senior/workflow/generator-typescript-jest-sdk/generators/app/templates/test/index.test.ts
+++ senior/workflow/generator-typescript-jest-sdk/generators/app/templates/test/index.test.ts
@@ -4,5 +4,6 @@ import Index from '../src/index'
 describe('Index Test', () => {
   test('create class, to be instance', (done) => {
     expect(new Index()).toBeInstanceOf(Index);
+    done();
   });
 })
