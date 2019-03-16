d1d7e350d776eb121093fff0fb42023a5f11b69c
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-03-16 12:44:43 +0800

    update

---
+++ base/mobx-counter/src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { observable, action } from "mobx";
+import { Provider, observer, inject } from "mobx-react";
+
+import "./styles.css";
+
+class Store {
+  @observable count = 0;
+
+  @action inc = (n = 1) => (this.count += n);
+}
+
+@inject("store")
+@observer
+class App extends React.Component {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <p onClick={() => store.inc()}>{store.count}</p>
+        <p onClick={() => store.inc(3)}>{store.count}</p>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={new Store()}>
+    <div>
+      <App />
+      <App />
+    </div>
+  </Provider>,
+  rootElement
+);
